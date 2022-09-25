afb908407ce6b3a09fd2821401f02b20c997c48d
Rostyslav Lysenko <educ@i.ua>
2022-09-26 00:55:35 +0300

    added api instance blank

--- internal/app/api/api.go
+++ internal/app/api/api.go
@@ -1,1 +1,15 @@
 package api
+
+// Base API server instance description
+type API struct {
+}
+
+// API constructor: build base API instance
+func New() *API {
+	return &API{}
+}
+
+// Start http server, configure loggers, router, database connection and etc...
+func (api *API) Start() error {
+	return nil
+}
