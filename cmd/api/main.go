21a2ad898cde134417b68a6ef3de807c1c83044e
Rostyslav Lysenko <educ@i.ua>
2022-09-26 01:01:57 +0300

    added server instance init&start

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/iamRosty/api-skeleton/internal/app/api"
+)
 
 var ()
 
@@ -9,5 +13,11 @@ func init() {
 }
 
 func main() {
-log.Printf("Ok")
+	log.Printf("Ok")
+	// server instance init
+	server := api.New()
+	// api server start
+	if err := server.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
